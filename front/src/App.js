ba003484c8f14c280df4c254cbd6a78ab0388954
eloise87 <eloise.marie@laposte.net>
2020-03-04 15:51:09 +0100

    ajout 2 photos carroussel

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import NavBar from "./components/NavBar/NavBar";
+import HomePage from './components/HomePage/HomePage'
 
 function App() {
     return (
         <div className="App">
-            <NavBar />
+            <HomePage />
         </div>
 
     );
--- front/src/components/HomePage/HomePage.js
+++ front/src/components/HomePage/HomePage.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import './HomePage.css';
 import NavBar from '../NavBar/NavBar';
-import Caroussel from '../Caroussel/Caroussel';
 
 
 const HomePage = () => {
     return (
         <div>
             <NavBar/>
-            <div className="Home">
-                <div className="slider">
-                    <div className="slide">
-                        <img src="http://fillmurray.com/300/300" alt="" className="img-responsive"/>
-                        <h1>slide 1</h1>
-                    </div>
-                    <div className="slide">
-                        <img src="http://fillmurray.com/300/300" alt="" className="img-responsive"/>
-                        <h1>slide 2</h1>
-                    </div>
-                    <div className="slide">
-                        <img src="http://fillmurray.com/300/300" alt="" className="img-responsive"/>
-                        <h1>slide 3</h1>
-                    </div>
-                    <div className="slide">
-                        <img src="http://fillmurray.com/300/300" alt="" className="img-responsive"/>
-                        <h1>slide 4</h1>
-                    </div>
-                    <div className="slide">
-                        <img src="http://fillmurray.com/300/300" alt="" className="img-responsive"/>
-                        <h1>slide 5</h1>
-                    </div>
-                </div>
-
-
+            <div className="slider">
+                <img src={require('../../assets/slide1.jpg')} alt="poignée de main"/>
             </div>
-            );
-            };
+            <div className="slider2">
+                <img src={require('../../assets/slide2.jpg')} alt="poignée de main"/>
+            </div>
+        </div>
+    );
+};
 
-            export default HomePage;
+export default HomePage;
