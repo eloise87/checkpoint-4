a0abcec44b3ab0413b70ce0068232e60c080ccbf
eloise87 <eloise.marie@laposte.net>
2020-03-04 13:33:35 +0100

    ajout navbar

---
+++ front/src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div className="NavBar">
+            <NavBar id="NavBar">
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                </ul>
+            </NavBar>
+        </div>
+    );
+};
+
+export default NavBar;
