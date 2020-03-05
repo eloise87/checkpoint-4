f363b17a463593730e8c9764893372406ed98914
eloise87 <eloise.marie@laposte.net>
2020-03-05 16:52:05 +0100

    css global

--- front/src/components/Footer/Footer.js
+++ front/src/components/Footer/Footer.js
@@ -5,12 +5,25 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="address">
-            10 rue du pot pourri 45000 Orléans
+            <div className="cont">
+                <div>
+                    <div className="address">
+                        10 rue du pot pourri 45000 Orléans
+                    </div>
+                    <div className="num">
+                        Tel : 02.38.02.01.00
+                    </div>
+                </div>
+            </div>
+            <div className="imageFooter">
+                <img
+                    className="element3"
+                    src={require('../../assets/slide2.jpg')}
+                    alt="element"/>
             </div>
             <div className="mentions">Mentions légales</div>
 
-            </div>
+        </div>
     );
 };
 
