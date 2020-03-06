7b1d5bf4162bb19728bcc1134e1ccf8c14a9e6f3
eloise87 <eloise.marie@laposte.net>
2020-03-06 10:59:07 +0100

    css déco

--- front/src/components/Deco/Deco.js
+++ front/src/components/Deco/Deco.js
@@ -6,78 +6,164 @@ import Footer from '../Footer/Footer';
 
 const Deco = () => {
 
-        return (
-            <div>
+    return (
+        <div>
 
-                <NavBar/>
+            <NavBar/>
 
-                <div className="generalTitle2">15 idées pour une fête zéro déchet !</div>
+            <div className="generalTitle2">15 idées pour une fête zéro déchet !</div>
 
-                <p>
+            <p>
 
-                    Qui n’a pas redouté le fameux moment de fin de soirée où l’on doit tout ranger, remplir les sacs poubelle pour jeter vaisselle, déco et tous les restes… Avec un peu de préparation en amont, on peut vraiment recevoir et s’amuser tout en réduisant son impact sur l’environnement. En en plus on va réduire le budget dépenses au passage ! Faites le plein de bonnes idées et découvrez ces 15 astuces pour organiser une belle fête zéro déchet.
-                    Déco, on mise à fond sur la récup’ !
+                Qui n’a pas redouté le fameux moment de fin de soirée où l’on doit tout ranger, remplir les sacs
+                poubelle pour jeter vaisselle, déco et tous les restes… Avec un peu de préparation en amont, on peut
+                vraiment recevoir et s’amuser tout en réduisant son impact sur l’environnement. En en plus on va réduire
+                le budget dépenses au passage ! Faites le plein de bonnes idées et découvrez ces 15 astuces pour
+                organiser une belle fête zéro déchet.
+                Déco, on mise à fond sur la récup’ ! </p>
+            <p>
+                A la chasse aux chutes de tissus et matériaux en bois ! La satisfaction du « do it yourself » et de
+                l’aspect esthétique seront réunis.
+                Déco de la salle.
+            </p>
+            <p>
 
-                    A la chasse aux chutes de tissus et matériaux en bois ! La satisfaction du « do it yourself » et de l’aspect esthétique seront réunis.
-                    Déco de la salle
+                • On fabrique une banderole en tissu, c’est facile ! Découvrez un joli tuto par ici.
+            </p>
 
-                    • On fabrique une banderole en tissu, c’est facile ! Découvrez un joli tuto par ici.
+            <p>
+                • On mise sur les matériaux de récup : palettes et tonneaux ont le vent en poupe et feront une jolie
+                décoration bohème ou champêtre par exemple.
 
-                    • On mise sur les matériaux de récup : palettes et tonneaux ont le vent en poupe et feront une jolie décoration bohème ou champêtre par exemple.
+            </p>
 
-                    • Les fleurs en papier, pourquoi pas ? A la seule condition de s’en resservir pour d’autres occasions !
-                    Déco des tables
+            <p>
+                • Les fleurs en papier, pourquoi pas ? A la seule condition de s’en resservir pour d’autres occasions !
+                Déco des tables.
+            </p>
 
-                    • Pour les nappes, on emprunte ou on chine des vieux draps blancs ou imprimés.
-                    On placera au centre un chemin de table en tissu ou toile de jute, effet garanti !
+            <p>
+                • Pour les nappes, on emprunte ou on chine des vieux draps blancs ou imprimés.
+                On placera au centre un chemin de table en tissu ou toile de jute, effet garanti !
 
-                    • Pensez aux cailloux colorés ou peints, aux plantes grasses, ramassez les pommes de pins et toutes matières naturelles qui pourront s’accorder avec votre thème.
+            </p>
+            <p>
+                • Pensez aux cailloux colorés ou peints, aux plantes grasses, ramassez les pommes de pins et toutes
+                matières naturelles qui pourront s’accorder avec votre thème.
+            </p>
+            <p>
+                • Et les serviettes de table ? On peut aussi éviter le papier. Sortez la machine à coudre : 50
+                serviettes en tissus à partir de chutes ne prendront que 2 heures de votre temps et si vous ne savez pas
+                enfiler une aiguille, demandez à belle-maman (bon faudra peut-être l’inviter du coup !) ou aux copines
+                😊
+            </p>
 
-                    • Et les serviettes de table ? On peut aussi éviter le papier. Sortez la machine à coudre : 50 serviettes en tissus à partir de chutes ne prendront que 2 heures de votre temps et si vous ne savez pas enfiler une aiguille, demandez à belle-maman (bon faudra peut-être l’inviter du coup !) ou aux copines 😊
-
-                    En panne d’idées ? Rendez-vous sur notre compte Pinterest, vous trouverez plein d’exemples dans tous les styles pour un anniversaire de 50 ans ou tout autre occasion.
-                    Pour les courses, place au vrac !
-                    L’apéritif
-
-                    Pour un apéro réussi, direction le marché, muni de ses sacs à vrac.
-                    Le choix en vrac est large : olives, pistaches et noix, tapenades… On prévoira si nécessaire en complément de préparer des légumes à couper en lamelles. Avec une mayo maison, des rillettes de poisson ou un guacamole, ça fonctionne !
-                    Les entrées ou plats
-
-                    • Pour la viande ou le poisson, on pense à emmener ses contenants en verre chez les commerçants. Cela évite de jeter ensuite les nombreuses barquettes.
-                    • En attendant le jour J, ne couvrez-pas vos plats avec du film plastique ou de l’alu mais plutôt avec des couvre-plats en tissu (à réaliser soi-même ou à chiner dans votre boutique bio !).
-                    Les boissons
-
-                    • Punch, marquisette ou autres mélanges, on les fait soi-même (à moins que Tom Cruise ne débarque dans votre salon comme dans Cocktails…) et on les dispose ensuite dans des fontaines en verre. Vous trouverez probablement dans votre entourage une personne qui pourra vous en prêter, sinon on aime bien celles de Maisons du Monde. Sinon, les grands seaux à champagne peuvent aussi servir de contenants.
-
-                    • Pour toutes les boissons en général, on peut remplir des cubis ou des bouteilles en verre chez les cavistes ou dans les boutiques bio. Ainsi, inutile de se rendre aux bacs de tri le lendemain avec une pyramide de bouteilles à jeter 😊 Ouf !
-                    Le point-clé, la vaisselle !
-
-                    On bannit définitivement les gobelets et pailles en plastique. Une multitude d’alternatives s’offrent à vous, entre le compostable ou la vaisselle comestible :
-                    • On peut opter pour de la vaisselle biodégradable en bambou par ici plutôt que des verres et assiettes jetables.
-                    Le choix est large entre assiettes, couverts, gobelets et même verrines !
-
-                    Dîtes adieu aux pailles en plastiques, il existe des pailles biodégradables (ou en métal).
-                    Moins de déchets et en plus votre compost sera content 😉
-
-                    • Autre solution pour les verres : optez pour des verres en plastiques épais que vous pourrez personnaliser aux couleurs de votre fête et offrir aux invités en souvenir. Sinon si vous n’avez pas de temps, laisser un feutre pour que chaque invité note son prénom sur son verre réutilisable. Ainsi, vous aurez moins de gaspillage.
-                    • Pour vos verrines ou le café, les couverts mangeables feront sensation !
-                    • Enfin, selon le nombre de personnes, on peut aussi emprunter un peu de vaisselle à des proches et mettre en marche le lave-vaisselle.
-                    Inciter ses invités à ne pas emballer les cadeaux et les fleurs
-
-                    Pour les anniversaires, en organisant une cagnotte sur place, on minimise le papier cadeau !
-                    Pour une cagnotte spéciale voyage, la bonne idée c’est le globe à décorer soi-même !
-                    On peut préciser tout cela gentiment de vive voix ou rajouter une petite mention « Esprit zéro déchet » sur les invitations.
-                    Et pour remercier ses invités, pensez à la carte végétale à planter qui laissera un joli souvenir !
-
-                </p>
+            <p>
+                En panne d’idées ? Rendez-vous sur notre compte Pinterest, vous trouverez plein d’exemples dans tous les
+                styles pour un anniversaire de 50 ans ou tout autre occasion.
+                Pour les courses, place au vrac !
+            </p>
 
 
-                <Footer/>
+            <h1> L’apéritif </h1>
 
-            </div>
+            <p>
 
-        );
-    }
-;
+                Pour un apéro réussi, direction le marché, muni de ses sacs à vrac.
+                Le choix en vrac est large : olives, pistaches et noix, tapenades… On prévoira si nécessaire en
+                complément de préparer des légumes à couper en lamelles. Avec une mayo maison, des rillettes de poisson
+                ou un guacamole, ça fonctionne !
 
-export default Deco;
+            </p>
+
+            <h1>
+                Les entrées ou plats
+
+            </h1>
+
+            <p>
+                • Pour la viande ou le poisson, on pense à emmener ses contenants en verre chez les commerçants. Cela
+                évite de jeter ensuite les nombreuses barquettes.
+            </p>
+
+            <p>
+                • En attendant le jour J, ne couvrez-pas vos plats avec du film plastique ou de l’alu mais plutôt avec
+                des couvre-plats en tissu (à réaliser soi-même ou à chiner dans votre boutique bio !).
+            </p>
+            <h1>
+                Les boissons </h1>
+
+            <p>
+
+                • Punch, marquisette ou autres mélanges, on les fait soi-même (à moins que Tom Cruise ne débarque dans
+                votre salon comme dans Cocktails…) et on les dispose ensuite dans des fontaines en verre. Vous trouverez
+                probablement dans votre entourage une personne qui pourra vous en prêter, sinon on aime bien celles de
+                Maisons du Monde. Sinon, les grands seaux à champagne peuvent aussi servir de contenants.
+            </p>
+
+            <p>
+                • Pour toutes les boissons en général, on peut remplir des cubis ou des bouteilles en verre chez les
+                cavistes ou dans les boutiques bio. Ainsi, inutile de se rendre aux bacs de tri le lendemain avec une
+                pyramide de bouteilles à jeter 😊 Ouf !
+            </p>
+
+            <h1> Le point-clé, la vaisselle ! </h1>
+
+            <p>
+
+                On bannit définitivement les gobelets et pailles en plastique. Une multitude d’alternatives s’offrent à
+                vous, entre le compostable ou la vaisselle comestible : </p>
+
+
+            <p>
+                • On peut opter pour de la vaisselle biodégradable en bambou par ici plutôt que des verres et assiettes
+                jetables. </p>
+
+            <p>
+
+                Le choix est large entre assiettes, couverts, gobelets et même verrines ! </p>
+
+
+            <p>
+                Dîtes adieu aux pailles en plastiques, il existe des pailles biodégradables (ou en métal).
+                Moins de déchets et en plus votre compost sera content 😉
+            </p>
+
+            <p>
+                • Autre solution pour les verres : optez pour des verres en plastiques épais que vous pourrez
+                personnaliser aux couleurs de votre fête et offrir aux invités en souvenir. Sinon si vous n’avez pas de
+                temps, laisser un feutre pour que chaque invité note son prénom sur son verre réutilisable. Ainsi, vous
+                aurez moins de gaspillage.
+            </p>
+
+            <p>
+                • Pour vos verrines ou le café, les couverts mangeables feront sensation !
+            </p>
+
+            <p>
+                • Enfin, selon le nombre de personnes, on peut aussi emprunter un peu de vaisselle à des proches et
+                mettre en marche le lave-vaisselle.
+            </p>
+
+            <p>
+                Inciter ses invités à ne pas emballer les cadeaux et les fleurs.
+            </p>
+            <p>
+                Pour les anniversaires, en organisant une cagnotte sur place, on minimise le papier cadeau !
+                Pour une cagnotte spéciale voyage, la bonne idée c’est le globe à décorer soi-même !
+                On peut préciser tout cela gentiment de vive voix ou rajouter une petite mention « Esprit zéro déchet »
+                sur les invitations.
+                Et pour remercier ses invités, pensez à la carte végétale à planter qui laissera un joli souvenir !
+
+            </p>
+
+
+            < Footer/>
+
+            < /div>
+
+                );
+                }
+                ;
+
+                export default Deco;
--- front/src/components/Sante/Sante.js
+++ front/src/components/Sante/Sante.js
@@ -21,27 +21,53 @@ const Sante = () => {
                     Prévenez le mal avec les plantes : infusion de fenouil pour la digestion, de menthe poivrée pour le
                     rhume, de queues de cerises pour un effet diurétique, de noyau d’avocats en cas de diarrhée, de sauge si
                     vous avez des règles douloureuses…
+
+                </p>
+
+                <p>
                     Utilisez les huiles essentielles avec parcimonie et assurez-vous que vous n’êtes pas allergique ni ne
                     présentez de contre-indication.
-                    Portez un bonnet et des gants pour isoler votre corps du froid
+
+                </p>
+
+                <p>
+                    Portez un bonnet et des gants pour isoler votre corps du froid.
+
+                </p>
+
+                <p>
                     Nettoyez les coupures et les égratignures à l’eau et au savon et laissez sécher à l’air libre (pas de
                     pansements).
+
+                </p>
+
+                <p>
                     Utilisez du fil dentaire : il vaut mieux ça qu’une carie !
+                </p>
+                <p>
 
                     Protégez-vous !
 
-                    Fabriquez votre crème solaire et protégez-vous des rayons avec des vêtements lors de la randonnée ou de
+                    Fabriquez votre crème solaire et protégez-vous des rayons avec des vêtements lors de la randonnée ou
+                    de
                     l’exposition aux heures les plus chaudes.
                     Pour du sexe (presque) zero dechet, utilisez des préservatifs véganes et des sextoys en matière
                     naturelle.
 
-                    Les médicaments
+                </p>
+
+
+                <p>
+
+                    Les médicaments :
 
                     Faites le tri de vos médicaments.
                     Un médicament ne doit pas être jeté dans une poubelle ménagère !
-                    Ramenez ceux qui sont périmés dans votre pharmacie : elle se chargera de la gestion. Ils sont incinérés
+                    Ramenez ceux qui sont périmés dans votre pharmacie : elle se chargera de la gestion. Ils sont
+                    incinérés
                     séparément des déchets ménagers, avec les DASRI (déchets d’hôpitaux).
-                    Si vous le pouvez, favorisez les médicaments dans des plaquettes en aluminium (que vous pourrez mettre
+                    Si vous le pouvez, favorisez les médicaments dans des plaquettes en aluminium (que vous pourrez
+                    mettre
                     au tri), ou dans des tubes en plastique de type /1\ ou /2\ (que vous pourrez mettre au tri).
                     Trier les boites et cartons et les notices en papier.
 
